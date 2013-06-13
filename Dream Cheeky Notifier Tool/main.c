72324b60823a03210208eebb534c113b5397e2fb
Philip Edelbrock <phil@Philips-MacBook-Pro-2.local>
2013-06-12 19:22:15 -0700

    Initial Commit

---
+++ Dream Cheeky Notifier Tool/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Dream Cheeky Notifier Tool
+//
+//  Created by Philip Edelbrock on 6/12/13.
+//  Copyright (c) 2013 Philip Edelbrock. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
